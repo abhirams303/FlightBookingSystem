1849fdff287599be41e5da7b00a031922b19f9dd
abhirams303 <116932504+abhirams303@users.noreply.github.com>
2022-10-29 11:56:43 +0530

    Add files via upload
    Added User Schema

---
+++ userSchema.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        default: []
+    },
+    passportNumber: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+    },
+    gender: {
+        type: String,
+    }
+},
+    { timestamps: true })
+const User = mongoose.model('User', userSchema);
+module.exports = User;
+
